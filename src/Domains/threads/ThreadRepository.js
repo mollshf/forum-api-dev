dcd8d3f11b3b1fddf2f7acae0eb8449e6392371e
M Rizki Maulana <kikymaulana00@gmail.com>
2024-03-24 22:13:41 +0700

    feat: add thread interface

---
+++ src/Domains/threads/ThreadRepository.js
@@ -0,0 +1,15 @@
+class ThreadRepository {
+  async addThread() {
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async getTheThreadById() {
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async verifyTheThread() {
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+}
+
+module.exports = ThreadRepository;
---
+++ src/Domains/threads/_test/ThreadRepository.test.js
@@ -0,0 +1,19 @@
+const ThreadRepository = require('../ThreadRepository');
+
+describe('ThreadRepository', () => {
+  it('should throw error when invoke abstract behavior', async () => {
+    // Arrange
+    const threaRepository = new ThreadRepository();
+
+    // Action & Assert
+    await expect(threaRepository.addThread({})).rejects.toThrow(
+      'THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED',
+    );
+    await expect(threaRepository.getTheThreadById('')).rejects.toThrow(
+      'THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED',
+    );
+    await expect(threaRepository.verifyTheThread('')).rejects.toThrow(
+      'THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED',
+    );
+  });
+});
